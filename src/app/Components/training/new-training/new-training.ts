de925abc619fbd0f054f59cf76650a3b831ea16b
Jerry Sparkman <jerry.sparkman@gmail.com>
2025-11-01 10:34:33 -0500

    Added new training tab

--- src/app/Components/training/new-training/new-training.ts
+++ src/app/Components/training/new-training/new-training.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-new-training',
-  imports: [],
+  imports: [MatCardModule, FlexLayoutModule, MatButtonModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './new-training.html',
   styleUrl: './new-training.sass'
 })
 export class NewTraining {
 
+  onAction1() {
+    console.log('Action1 clicked');
+  }
 }
--- src/app/Components/training/training/training.ts
+++ src/app/Components/training/training/training.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NewTraining } from "../new-training/new-training";
+import { CurrentTraining } from "../current-training/current-training";
+import { PastTraining } from '../past-training/past-training';
 
 @Component({
   selector: 'app-training',
-  imports: [],
+  imports: [MatTabsModule, NewTraining, CurrentTraining, PastTraining],
   templateUrl: './training.html',
   styleUrl: './training.sass'
 })
